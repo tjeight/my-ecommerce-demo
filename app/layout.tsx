6fd5b32bdfcfbc4bd5cbdc0f780be81dc3e3130b
Tejas Jagdale <thetejasjagdale@gmail.com>
2025-06-04 17:39:22 +0530

    build issues fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";   
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
